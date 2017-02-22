6941b3345f7142f365d5359603100f2fbffaaec4
Zac Mitton <zacmitton22@gmail.com>
2017-02-21 19:09:50 -0500

    WIP singleton controller

--- contracts/controllers/SingletonStandardController.sol
+++ contracts/controllers/SingletonStandardController.sol
@@ -7,32 +7,34 @@ contract SingletonStandardController {
 
   mapping(address => Identity) public identities;//userKey->proxyaddress
   struct Identity{
-    bool    hasAccess;
-    Proxy   proxy;
+    bool    public isInit;
+    address public proxyAddress;
     address public recoveryKey;
     uint64  public unlockedAt; //0x0 for always locked
   }
 
   event RecoveryEvent(string action, address initiatedBy);
-  modifier onlyRecoveryKey(userKey){ if(identities[userKey].recoveryKey == msg.sender) _; }
+  modifier onlyRecoveryKey(userKey){ if(identities[userKey].recoveryKey == msg.sender)_; }
+  modifier onlyUnlocked(userKey){ if(!locked(identities[userKey]) _; }
 
   function SingletonStandardController(uint _timeLock) {
     version = 1;
     timeLock = _timeLock;
   }
-
+//creation
   function registerProxy(address recoveryKey, uint8 v, bytes32 r, bytes32 s) { //only proxy 
     address userKey = ecrecover(bytes32(msg.sender), v, r, s);
     //userKey is 0x0 on sig failure. Do we need to protect this (zero address)?
-    if(!locked(identities[userKey])){
-      identities[userKey] = Identity({
-        proxy: Proxy(msg.sender), 
-        recoveryKey: recoveryKey, 
-        unlockedAt: 0x0
-      });
-    }
+    create(userKey, recoveryKey);
   }
-
+  function create(address userKey, address recoveryKey) private onlyUnlocked(userKey){
+    identities[userKey] = Identity({
+      isInit: true,
+      proxyAddress: msg.sender, 
+      recoveryKey: recoveryKey, 
+      unlockedAt: 0x0
+    });
+  }
   function locked(Identity id) private returns(bool){
     if(id.userKey != 0x0){ //user exists
       if(id.settingsUnlocked >= now || id.settingsUnlocked == 0x0){ //settings are locked
@@ -41,23 +43,21 @@ contract SingletonStandardController {
     }
     return false;
   }
-
+//use
   function forward(address destination, uint value, bytes data) {
-    identities[msg.sender].proxy.forward(destination, value, data);
+    Proxy(identities[msg.sender].proxyAddress).forward(destination, value, data);
   }
-
+//settings
   function unlock(){
     identities[msg.sender].unlockedAt = now + timeLock;
   }
-  function changeRecovery() { 
-    identities[msg.sender].recoveryKey = recoveryKey; 
+  function changeController(address controller) onlyUnlocked(msg.sender){ 
+    proxy.transfer(controller); 
     delete identities[oldUserKey].unlockedAt;
   }
-  function changeController(address controller) {
-    if(!locked(identities[msg.sender])){ 
-      proxy.transfer(controller); 
-      delete identities[oldUserKey].unlockedAt;
-    }
+  function changeRecovery() onlyUnlocked(msg.sender){ 
+    identities[msg.sender].recoveryKey = recoveryKey; 
+    delete identities[oldUserKey].unlockedAt;
   }
   function changeRecoveryFromRecovery(address userKey, address newRecoveryKey) onlyRecoveryKey(userKey){ 
     identities[userKey].recoveryKey = newRecoveryKey; 
@@ -68,4 +68,3 @@ contract SingletonStandardController {
     delete identities[oldUserKey].unlockedAt;
   }
 }
-
