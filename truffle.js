a619f8615e694ac0f6d88a68f1a0d471d40d44d3
Zac Mitton <zacmitton22@gmail.com>
2017-04-17 18:11:23 -0500

    deployed new idfactory with creatable proxy to kovan and ropsten

--- truffle.js
+++ truffle.js
@@ -1,3 +1,9 @@
+var Web3 = require('web3');
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = require('./testMnemonics')
+
+// var provider = new HDWalletProvider(mnemonic.ropsten, "https://infuranet.infura.io/");
+// var web3 = new Web3(provider)
 
 
 module.exports = {
@@ -10,7 +16,15 @@ module.exports = {
     ropsten: {
       host: 'localhost',
       port: 8545,
-      network_id: 3
+      network_id: 3,
+      provider: new HDWalletProvider(mnemonic.ropsten, "https://ropsten.infura.io/")
+    },
+    kovan: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 42,
+      gasLimit: 4712000,
+      provider: new HDWalletProvider(mnemonic.kovan, "https://kovan.infura.io/")
     },
     ethereum: {
       host: 'localhost',
